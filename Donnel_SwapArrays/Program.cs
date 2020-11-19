526592d2035f01afe2daea80adb9eb813bbd59de
Donnel <donnel206@hotmail.com>
2020-11-19 09:43:33 +0100

    Add project files.

---
+++ Donnel_SwapArrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donnel_SwapArrays
+{
+    class Program
+    {
+
+       
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            string[] Array1 = { "1", "2", "3", "4", "5" };
+            string[] Array2 = { "6", "8", "7", "10", "7" };
+            string[] tempArray = { };
+
+
+            tempArray = Array1
+
+
+            Console.WriteLine("");
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+        }
+    }
+}
